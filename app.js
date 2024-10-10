72d70b4bd73e37dbb132ecaa45fb0873c9aecd4b
Zeeshan Islam <159948421+Rohaan001@users.noreply.github.com>
2024-10-10 23:06:13 +0500

    add

---
+++ app.js
@@ -0,0 +1,50 @@
+// task 1 . string to capitalize using prompt
+// every letter capitalize
+
+
+// var pr = prompt("Enter a word ");
+
+// var first = pr.split(" ");
+
+// var arr = []
+
+// for (let i = 0; i < first.length; i++) {
+//     var a = first[i].slice(0,1).toUpperCase();
+//     var b = first[i].slice(1);
+//     var result = a + b;
+//     arr.push(result)
+// }
+// console.log(arr.join(" "));
+
+// let input = prompt("Enter a string:");
+// let capitalizedString = input.toUpperCase();
+// console.log(capitalizedString);
+
+
+
+// task 2 . in a paragraph find smit using slice
+// var text = "hello i am saylani mass it training student"
+// for (var i = 0; i < text.length; i++) {
+//    if (text.slice(i, i+7) ==="saylani") {
+//        document.write("saylani found")
+//     }
+//    else{
+//     document.write("saylani not found")
+//    }
+//     break;
+   
+// }
+
+// task03
+
+// var para = "This is an example paragraph. Saylani is mentioned here.";
+
+
+
+
+
+// // TASK 04 
+
+// var text = " hello i'm a student of saylani malir campus karachi ";
+// var word = text.replace("saylani","SMIT");
+// console.log(word);
